0e024dcd1a8cb374984b73ec0d59e8491f6e50e2
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2022-07-28 16:53:30 -0300

    crear primeras clases del modelo

---
+++ front/js/modelo/Clases.js
@@ -0,0 +1,61 @@
+class Name{
+    constructor(first,last){
+        this.first=first
+        this.last=last
+    }
+} 
+ class Documento{
+    constructor(tipo,numero){
+        this.tipo=tipo
+        this.numero=numero
+    }
+ }
+ class Domicilio
+ {
+constructor(
+provincia,localidad,barrio,direccion,codigoPostal
+){
+this.provincia=provincia
+this.localidad=localidad
+this.barrio=barrio
+this.direccion=direccion
+this.codigoPostal=codigoPostal
+}
+ }
+ class Celular {
+constructor(
+codigoPais,codigoArea,numero
+){
+this.codigoPais=codigoPais
+this.codigoArea=codigoArea
+this.numero=numero
+}
+ }
+ class Email  {
+constructor(
+direccion
+){
+//todo: separar dominio de direccion
+}
+ }
+ class Usuario {
+constructor(
+
+){
+
+}
+ }
+ class PaperCanvas {
+constructor(
+
+){
+
+}
+ }
+ class SpriteLoader {
+constructor(
+
+){
+
+}
+ }
